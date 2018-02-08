0773e743424bed6d6f54dac1a6bf2e12167cdd39
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2018-02-08 19:21:07 +0100

    added node functionality

--- _controllers/external-controller.js
+++ _controllers/external-controller.js
@@ -1,6 +1,11 @@
 
 // show homepage
 exports.index = function(req, res){
+	// test contents
+	let contents = {
+		date: '2018'
+	}
+
 	// load welcome page
-	res.render('welcome');
+	res.render('welcome', {contents: contents});
 }
