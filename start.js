73fcc40b48463a8f00f0c38d7e44a3c8204cdcde
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2018-02-09 12:01:31 +0100

    .evn file alter

--- start.js
+++ start.js
@@ -5,13 +5,13 @@ let app = require('./app');
 // let database = require('./database/database');
 
 // set application port
-let PORT = process.env.DB_PORT || 5000;
-let HOST = "0.0.0.0";
+let PORT = process.env.PORT || 3000;
+
 // start application server
-app.listen(PORT, HOST, function (){
+app.listen(PORT, function (){
 	console.log('Launching noravel');
 	console.log("Server starting....");
-	console.log("Server running: http://localhost:" + PORT);
+	console.log("Server running: http://localhost:"+PORT);
 	console.log("Press Ctrl+C to terminate server");
 	console.log("----------(^±^)----------");
 });
