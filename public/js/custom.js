659b9a766e3d056e035eab519ad2f4e9eb7a868e
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2018-04-16 21:43:11 +0100

    added person class

---
+++ public/js/custom.js
@@ -0,0 +1,25 @@
+/*
+*--------------------------
+* Testing javascript class
+*--------------------------
+*
+*/
+
+class Person{
+
+	// init constructor
+	constructor(name){
+
+		this.name = name;
+	}
+
+	// greet user
+	greet(){
+		console.log(this.name+ ' Say hello');
+	}
+
+	// save information
+	AddUser(){
+		
+	}
+}
